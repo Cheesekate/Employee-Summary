1b360fa015becfc5948e75ce6c7882f48982196a
Kathryn wade <kathrynwade@Kathryns-MacBook.local>
2020-06-09 16:25:13 -0700

    updates to app.js

--- Develop/app.js
+++ Develop/app.js
@@ -14,6 +14,35 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function createTeam() {
+    inquirer
+        .prompt([
+            {
+                name: "addMember",
+                type: "list",
+                message: "Would you like to add a team member?",
+                choices: [
+                    "Yes",
+                    "No my team is complete",
+                ],
+
+            },
+        ])
+        .then(function (data) {
+            switch (data.addMember) {
+                case "Yes, add to the Team":
+                    renderer();
+                    console.log(hello);
+                    break;
+                case "No my team is complete":
+                    buildTeam();
+                    break;
+            }
+        });
+}
+
+
+
 
 function renderer() {
     inquirer.prompt([
@@ -50,6 +79,17 @@ function renderer() {
                         message: "What is your github?",
                     },
                 ]);
+                (function (result) {
+                    console.log(result);
+                    const newEngineer = new Engineer(
+                        result.name,
+                        result.id,
+                        result.email,
+                        result.github
+                    );
+                    teamMembers.push(newEngineer);
+                    createTeam();
+                })
             };
             if (answers === Manager) {
                 prompt([
@@ -59,6 +99,17 @@ function renderer() {
                         message: "What is your office number?",
                     },
                 ]);
+                (function (result) {
+                    console.log(result);
+                    const newManager = new Manager(
+                        result.name,
+                        result.id,
+                        result.email,
+                        result.number
+                    );
+                    teamMembers.push(newManager);
+                    createTeam();
+                });
             };
             if (answers === Intern) {
                 prompt([
@@ -69,8 +120,25 @@ function renderer() {
                     },
                 ]);
             };
+            (function (result) {
+                const newIntern = new Intern(
+                    result.name,
+                    result.id,
+                    result.email,
+                    result.school
+                );
+                teamMembers.push(newIntern);
+                createTeam();
+            });
         });
+
+
 
 };
 
-renderer();
+createTeam();
+
+
+function buildTeam() {
+    fs.writeFileSync(outputPath, render(teamMembers), "utf8");
+}
